44247c45ff0859e30ca3c4b486cc6f6fee7db514
virag <weilervirag@gmail.com>
2023-11-28 12:29:20 +0100

    edited the basic_cleaning step with boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -40,6 +40,9 @@ def go(args):
     logger.info("Fixing last_review type")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
 
